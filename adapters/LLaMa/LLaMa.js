edcb6c6f39316edad39c88923f6145a4f5765a92
Herman Liang <herman@koii.network>
2024-10-22 21:29:13 -0300

    LLaMa updated

--- adapters/LLaMa/LLaMa.js
+++ adapters/LLaMa/LLaMa.js
@@ -1,5 +1,32 @@
+async function getEndpoints(){
+    const endpoints = await fetch("https://vps-tasknet.koii.network/nodes/7ia22HzfZHPXh8kKwvcyEvHBEg1EaQvUJARUzejYbJQv")
+    const endpointsList = (await endpoints.json()).map(node => node.data.url);
+    console.log(endpointsList);
+    return endpointsList;
+}
 async function askllama(question) {
-    return false;
+    const endpoints = await getEndpoints();
+    console.log(question);
+    // shuffle the endpoints
+    const shuffledEndpoints = endpoints.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < shuffledEndpoints.length; i++) {
+        const randomEndpoint = shuffledEndpoints[i];
+        const accessLink = randomEndpoint + "/task/7ia22HzfZHPXh8kKwvcyEvHBEg1EaQvUJARUzejYbJQv";
+        const response = await fetch(`${accessLink}/ask-query`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ model: "llama3.2", query: question }) 
+          });
+        try{
+            const data = await response.json();
+            const reply = data.reply;   
+            console.log(reply);
+            if (!reply) continue;
+            return reply;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = { askllama };
